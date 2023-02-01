f7b8c6f158af6287fe5aa0bf3a6bc65c93be6d0a
Alessandro Gattuso <alessandrogattuso@yahoo.it>
2023-02-01 22:29:11 +0100

    setDone fixed

--- server/getData.php
+++ server/getData.php
@@ -3,28 +3,22 @@
   $todolist = json_decode(file_get_contents('../data/todo-list.json'), true);
   
   if(isset($_POST['addTask']) &&  trim($_POST['addTask']) != ''){
-
     $todolist[] = [
         "task" => $_POST['addTask'],
         "done" => false,
     ];
-
-    file_put_contents('../data/todo-list.json', json_encode($todolist, JSON_PRETTY_PRINT));
   }
 
-  if(isset($_POST['deleteTask'])){
+  if(isset($_POST['deleteTask']))
     unset($todolist[$_POST['deleteTask']]);
-    file_put_contents('../data/todo-list.json', json_encode($todolist, JSON_PRETTY_PRINT));
-  }
+  
 
-  if(isset($_POST['setDone'])){
-    foreach($todolist as $key => $value){
-      if($value['task'] == $_POST['setDone'])
-          $todolist[$key]['done'] = !$value['done'];
-    }
-    file_put_contents('../data/todo-list.json', json_encode($todolist, JSON_PRETTY_PRINT));
-  }
+  if(isset($_POST['setDone']))
+    $todolist[$_POST['setDone']]['done'] = !$todolist[$_POST['setDone']]['done'];
+  
 
+  file_put_contents('../data/todo-list.json', json_encode($todolist, JSON_PRETTY_PRINT));
+  
   header('Content-Type: application/json');
   
   echo json_encode($todolist);
